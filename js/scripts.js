c4481847284b3dcf2b01e5faa4cef79e1584d1dc
Babex1 <anatolii.babeshko@gmail.com>
2022-07-27 14:32:08 +0300

    section4 done

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:0,
+        dots:true,
         nav:true,
         autoplay:true,
         rewind: false,
